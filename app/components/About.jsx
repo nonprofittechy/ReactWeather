ab7caa64e1d19e343e0792d05331a9986808f1a0
Quinten Steenhuis <qsteenhuis@gmail.com>
2016-08-24 15:30:11 -0400

    Updated About copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 // refactoring to use stateless component because only render method is used
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+      <div>
+          <h3>About</h3>
+          <p>This is a little information about this tutorial app.</p>
+      </div>
     );
 };
 
