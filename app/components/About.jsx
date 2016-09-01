c3c5458a446668de802c792a80afe409e6a59930
Quinten Steenhuis <qsteenhuis@gmail.com>
2016-09-01 16:11:16 -0400

    Added more content to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,27 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // refactoring to use stateless component because only render method is used
 var About = (props) => {
   return (
       <div>
-          <h3>About</h3>
-          <p>This is a little information about this tutorial app.</p>
+          <h1 className="text-center">About</h1>
+          <p>
+            This is a little information about this tutorial app.
+          </p>
+          <p>
+            Here are some of the tools that I used.
+            <ul>
+              <li>
+                <a href="https://facebook.github.io/react">React</a> -
+                  This was the Javascript framework used.
+              </li>
+              <li>
+                <Link to="http://openweather.org">openWeatherMap</Link> -
+                  I used OpenWeatherMap to search for weather data by city name.
+              </li>
+            </ul>
+          </p>
       </div>
     );
 };
